4c27597b239179558252b018a42fae0be269c034
yayang12 <122055034+yayang12@users.noreply.github.com>
2024-02-02 14:32:18 -0800

    Finished Platformer check 2

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,6 +8,7 @@ const JUMP_VELOCITY = -300.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var coyote_jump_timer = $CoyoteJumpTimer
 
 
 func _physics_process(delta):
@@ -21,7 +22,11 @@ func _physics_process(delta):
 	handle_acceleration(input_axis, delta)
 	apply_friction(input_axis, delta)
 	update_animations(input_axis)
+	var was_on_floor = is_on_floor()
 	move_and_slide()
+	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0 
+	if just_left_ledge:
+		coyote_jump_timer.start()
 
 	
 func apply_gravity(delta):
@@ -29,10 +34,10 @@ func apply_gravity(delta):
 		velocity.y += gravity * delta
 		
 func handle_jump():
-	if is_on_floor():
+	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
 		if Input.is_action_just_pressed("ui_up"):
 			velocity.y = JUMP_VELOCITY
-	else:
+	if not is_on_floor():
 		if Input. is_action_just_released("ui_up") and velocity.y < JUMP_VELOCITY / 2:
 			velocity.y = JUMP_VELOCITY / 2
 			
