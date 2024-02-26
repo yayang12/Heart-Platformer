78c67087c744a2fe07db76b5047a1607207edb79
vivianw1 <122054036+vivianw1@users.noreply.github.com>
2024-02-25 21:02:36 -0800

    11 comp

--- scripts/world.gd
+++ scripts/world.gd
@@ -11,6 +11,7 @@ func _ready():
 func show_level_completed():
 	level_completed.show()
 	get_tree().paused = true
+	await get_tree().create_timer(1.0).timeout
 	if not next_level is PackedScene: return
 	await LevelTransition.fade_to_pink()
 	get_tree().paused = false
